bd6acf0063ff7f44a560a4d1c3d562088c3056f8
Jeroen Janssen <jeroen@trackuity.com>
2016-05-05 18:22:08 +0200

    Bump spaws to v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 kwargs = {
     "name": "spaws",
-    "version": "0.1",
+    "version": "0.2",
     "packages": find_packages(exclude=["tests", "tests.*"]),
     "install_requires": ["setuptools", "boto", "click"],
     "test_suite": "nose.collector",
